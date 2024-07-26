c90fbb8829b5b4f8dd3f0376cc13d7d5a1a59c60
AC-1300 <ac4984@nau.edu>
2024-07-26 10:07:56 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+//This prompts the user to enter their name
+function promptUser() {
+    const username = prompt("Please enter your name.");
+    if (username) {
+        document.getElementById('username').innerText = username;
+    }
+    
+}
+//function for calculating the percentage
+function CalculateTaskPercentage() {
+    const tasks = document.querySelectorAll('#task-list li');
+    const completedTasks = document.querySelectorAll('#task-list li input[type="checkbox"]:checked');
+
+    const totalTasks = tasks.length;
+    const completedCount = completedTasks.length;
+
+    const percentage = totalTasks === 0 ? 0 : Math.round((completedCount / totalTasks) * 100);
+    document.getElementById('task-percentage').textContent = percentage;
+}
+
+
+
+//this function will be used to create new tasks
+function NewTask(){
+    const taskList = document.getElementById('task-list');
+    const taskInput = document.getElementById('new-task');
+    const taskText = taskInput.value.trim();
+
+if (taskText !== '') {
+    const listItem = document.createElement('li');
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.onchange = CalculateTaskPercentage;
+
+    const label = document.createElement('label');
+    label.textContent = taskText;
+
+   
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.onclick = function() {
+        deleteTask(listItem);
+    };
+
+    listItem.appendChild(checkbox);
+    listItem.appendChild(label);
+    listItem.appendChild(deleteButton);
+
+    taskList.appendChild(listItem);
+    taskInput.value = '';
+
+    CalculateTaskPercentage();
+ }
+ else {
+    alert('Please enter task')
+ 
+}
+}
+
+//Delete a task
+function deleteTask(taskItem) {
+    const taskList = document.getElementById('task-list');
+    taskList.removeChild(taskItem);
+    CalculateTaskPercentage();
+}
+
